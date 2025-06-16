1d3e81d3e7eff55c60bdfa841400529915b73f74
Michael Pishchagin <mblsha+gh@gmail.com>
2025-06-16 11:21:08 +1000

    Add factory functions

---
+++ src/pyscumm6/instr/factories.py
@@ -0,0 +1,106 @@
+"""Smart factory functions for generating instruction classes."""
+
+from typing import Type, Dict, List
+from binja_helpers.tokens import Token
+
+from .opcodes import Instruction
+from .smart_bases import (SmartIntrinsicOp, SmartVariableOp, SmartArrayOp, SmartComplexOp, 
+                         SmartStackOp, SmartBinaryOp, SmartUnaryOp, SmartComparisonOp)
+from .configs import (IntrinsicConfig, VariableConfig, ArrayConfig, ComplexConfig, StackConfig,
+                     INTRINSIC_CONFIGS, VARIABLE_CONFIGS, ARRAY_CONFIGS, COMPLEX_CONFIGS, STACK_CONFIGS)
+
+def create_intrinsic_instruction(name: str, config: IntrinsicConfig) -> Type[Instruction]:
+    """Create an intrinsic instruction class from configuration."""
+    
+    class GeneratedIntrinsicOp(SmartIntrinsicOp):
+        _name = name
+        _config = config
+        __doc__ = config.doc
+        
+        def render(self) -> List[Token]:
+            return super().render()
+    
+    GeneratedIntrinsicOp.__name__ = name.title().replace("_", "")
+    GeneratedIntrinsicOp.__qualname__ = name.title().replace("_", "")
+    return GeneratedIntrinsicOp
+
+def create_variable_instruction(name: str, config: VariableConfig) -> Type[Instruction]:
+    """Create a variable operation instruction class from configuration."""
+    
+    class GeneratedVariableOp(SmartVariableOp):
+        _name = name
+        _config = config
+        __doc__ = config.doc
+    
+    GeneratedVariableOp.__name__ = name.title().replace("_", "")
+    GeneratedVariableOp.__qualname__ = name.title().replace("_", "")
+    return GeneratedVariableOp
+
+def create_array_instruction(name: str, config: ArrayConfig) -> Type[Instruction]:
+    """Create an array operation instruction class from configuration."""
+    
+    class GeneratedArrayOp(SmartArrayOp):
+        _name = name
+        _config = config
+        __doc__ = config.doc
+    
+    GeneratedArrayOp.__name__ = name.title().replace("_", "")
+    GeneratedArrayOp.__qualname__ = name.title().replace("_", "")
+    return GeneratedArrayOp
+
+def create_complex_instruction(name: str, config: ComplexConfig) -> Type[Instruction]:
+    """Create a complex operation instruction class from configuration."""
+    
+    class GeneratedComplexOp(SmartComplexOp):
+        _name = name
+        _config = config
+        __doc__ = config.doc
+    
+    GeneratedComplexOp.__name__ = name.title().replace("_", "")
+    GeneratedComplexOp.__qualname__ = name.title().replace("_", "")
+    return GeneratedComplexOp
+
+def create_stack_instruction(name: str, config: StackConfig) -> Type[Instruction]:
+    """Create a stack operation instruction class from configuration."""
+    
+    if config.is_comparison:
+        base_class = SmartComparisonOp
+    elif config.is_unary:
+        base_class = SmartUnaryOp
+    else:
+        base_class = SmartBinaryOp
+    
+    class GeneratedStackOp(base_class):
+        _name = name
+        _config = config
+        __doc__ = config.doc
+    
+    GeneratedStackOp.__name__ = name.title().replace("_", "")
+    GeneratedStackOp.__qualname__ = name.title().replace("_", "")
+    return GeneratedStackOp
+
+def generate_all_instructions() -> Dict[str, Type[Instruction]]:
+    """Generate all instruction classes from configurations."""
+    registry = {}
+    
+    # Generate intrinsic instructions
+    for name, config in INTRINSIC_CONFIGS.items():
+        registry[name] = create_intrinsic_instruction(name, config)
+    
+    # Generate variable instructions  
+    for name, config in VARIABLE_CONFIGS.items():
+        registry[name] = create_variable_instruction(name, config)
+    
+    # Generate array instructions
+    for name, config in ARRAY_CONFIGS.items():
+        registry[name] = create_array_instruction(name, config)
+    
+    # Generate complex instructions
+    for name, config in COMPLEX_CONFIGS.items():
+        registry[name] = create_complex_instruction(name, config)
+    
+    # Generate stack instructions
+    for name, config in STACK_CONFIGS.items():
+        registry[name] = create_stack_instruction(name, config)
+    
+    return registry
