25c875f08369cf3dd2c51c937959d5ba3a97f159
Michael Pishchagin <mblsha+gh@gmail.com>
2025-06-29 10:54:30 +1000

    fix: Change putActorAtXY to putActorInXY to match descumm
    - Update DESCUMM_FUNCTION_NAMES mapping in smart_bases.py
    - descumm uses 'putActorInXY' not 'putActorAtXY' for this function

--- src/pyscumm6/instr/smart_bases.py
+++ src/pyscumm6/instr/smart_bases.py
@@ -25,7 +25,7 @@ DESCUMM_FUNCTION_NAMES = {
     "animate_actor": "animateActor",
     "walk_actor_to": "walkActorTo",
     "cutscene": "beginCutscene",
-    "put_actor_at_xy": "putActorAtXY",
+    "put_actor_at_xy": "putActorInXY",
     "put_actor_in_xy": "putActorInXY",
     "face_actor": "faceActor",
     "start_sound": "startSound",
